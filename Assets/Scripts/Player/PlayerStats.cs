e626889881996532da618bb6630a596183e88bf6
emaestro <eduard.maestro@elma.care>
2021-02-09 20:30:45 +0100

    i dont really know hehehe. joke, added  CharacterStats class and strength stats to the player

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -6,16 +6,33 @@ using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
-    public int currentHealth = 0;
-    public int maxHealth = 100;
 
-    [SerializeField] private int expPoints = 0;
+
 
     public HealthBar healthBar;
 
+    public int currentHealth = 0;
+    public int maxHealth = 100;
+    [SerializeField] private int expPoints = 0;
+    [SerializeField] private CharacterStat armor = new CharacterStat(1);
+    [SerializeField] private CharacterStat strength = new CharacterStat(10);
+
+    private LevelSystem levelSystem;
+
+    private void Awake()
+    {
+
+    }
     void Start()
     {
+        maxHealth = 100 + Mathf.RoundToInt(strength.Value); ;
         currentHealth = maxHealth;
+    }
+    public void SetLevelSystem(LevelSystem levelSystem)
+    {
+        this.levelSystem = levelSystem;
+
+        // levelSystem.onLevelChanged
     }
 
     public float GetHealthPercentage()
@@ -32,12 +49,10 @@ public class PlayerStats : MonoBehaviour
 
     private void Update()
     {
-
         if (currentHealth <= 0)
         {
             this.PlayerDied();
         }
-
     }
 
     public void addExperience(int expPoints)
@@ -46,6 +61,7 @@ public class PlayerStats : MonoBehaviour
     }
     public void TakeDamage(int damage)
     {
+        damage = damage - Mathf.RoundToInt(armor.Value);
         if (damage <= 0) return;
         currentHealth -= damage;
 
---
+++ Assets/Scripts/Stats/CharacterStat.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+[Serializable]
+public class CharacterStat
+{
+    public float BaseValue;
+    public float Value
+    {
+        get
+        {
+            if (isDirty || BaseValue != lastBaseValue)
+            {
+                lastBaseValue = BaseValue;
+                _value = calculateFinalValue();
+                isDirty = false;
+            }
+            return calculateFinalValue();
+        }
+    }
+    protected bool isDirty = true;
+    protected float _value;
+    protected float lastBaseValue = float.MinValue;
+
+
+    protected readonly List<StatModifier> statModifiers;
+    public readonly ReadOnlyCollection<StatModifier> StatModifiers;
+
+    public CharacterStat()
+    {
+        statModifiers = new List<StatModifier>();
+        StatModifiers = statModifiers.AsReadOnly();
+    }
+    public CharacterStat(float baseValue) : this()
+    {
+        BaseValue = baseValue;
+    }
+
+    public virtual void AddModifier(StatModifier modifier)
+    {
+        isDirty = true;
+        statModifiers.Add(modifier);
+        statModifiers.Sort(CompareModifiersOrder);
+    }
+    protected virtual int CompareModifiersOrder(StatModifier a, StatModifier b)
+    {
+        if (a.order < b.order)
+            return -1;
+        else if (a.order > b.order) return 1;
+        else return 0;
+    }
+    public virtual bool RemoveModifier(StatModifier modifier)
+    {
+        if (statModifiers.Remove(modifier))
+        {
+            isDirty = true;
+            return true;
+        };
+        return false;
+    }
+    public virtual bool RemoveModifierFromSource(object source)
+    {
+        bool didRemove = false;
+        for (int i = statModifiers.Count - 1; i >= 0; i--)
+        {
+            if (statModifiers[i].source == source)
+            {
+                isDirty = true;
+                didRemove = true;
+                statModifiers.RemoveAt(i);
+            }
+        }
+        return didRemove;
+    }
+    protected virtual float calculateFinalValue()
+    {
+        float finalValue = BaseValue;
+        float sumPercentAdd = 0;
+
+        for (int i = 0; i < statModifiers.Count; i++)
+        {
+            StatModifier mod = statModifiers[i];
+            if (mod.type == StatModType.Flat)
+            {
+                finalValue += statModifiers[i].value;
+            }
+            else if (mod.type == StatModType.PercentMult)
+            {
+                sumPercentAdd += mod.value;
+                if (i + 1 >= statModifiers.Count || statModifiers[i + 1].type != StatModType.PercentAdd)
+                {
+                    finalValue *= 1 + sumPercentAdd;
+                    sumPercentAdd = 0;
+                }
+            }
+            else if (mod.type == StatModType.PercentAdd)
+            {
+                finalValue *= 1 + mod.value;
+            }
+
+        }
+        return (float)Mathf.Round(finalValue);
+    }
+}
